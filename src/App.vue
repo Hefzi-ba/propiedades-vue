0efcaabe858dd9bd7cadf45a0979295b8ebdf3aa
Hefzi-ba <bamurillo15@gmail.com>
2022-10-04 16:19:04 -0600

    Propiedades

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MiPrimerComponente></MiPrimerComponente>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+import MiPrimerComponente from './components/MiPrimerComponente.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    
+    MiPrimerComponente
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MiPrimerComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{counter}}</h1>
+        <button @click="counter++">+1</button>
+        <button @click="counter--">-1</button>
+        <button @click="counter=counter+5">+5</button>
+        <button @click="counter=counter-5">-5</button>
+        
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                counter:0
+            }
+        }
+    }
+</script>
